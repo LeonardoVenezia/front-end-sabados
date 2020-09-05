50fb75da9d6ac6e8dfd3e78782bea26d076c2ea1
Leonardo <leonardo.venezia.contractor@gdevtools.com>
2020-09-05 13:05:43 -0300

    ajax baby!!!

--- clase03/index.js
+++ clase03/index.js
@@ -1,37 +1,56 @@
 /// BUBBLE Y CAPTURING
-/* 
+
 const uno = document.querySelector('#uno');
 const dos = document.querySelector('#dos');
 const tres = document.querySelector('#tres');
 
 uno.addEventListener('click', (e)=> {
-    e.stopPropagation()
-    console.log(e.target)
-    console.log('UNO')
+    callbackUno(e, "parametro")
 }, true )
 dos.addEventListener('click', (e)=> {
-    e.stopPropagation()
+    // e.stopPropagation()
     console.log(e.target)
     console.log('DOS')
 }, true )
 tres.addEventListener('click', (e)=> {
-    e.stopPropagation()
+    // e.stopPropagation()
     console.log(e.target)
     console.log('TRES')
 }, true )
 
-*/
+function callbackUno(e, arg) {
+    e.stopPropagation()
+    // e.preventDefault()
+    console.log(e)
+    console.log(e.target)
+    console.log('UNO')
+    console.log(arg)
+}
+
+
 // Funciones iteradoras
 
 const dias = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
 
-// const diasMapeados = dias.map((item)=> {
-//     return `<p>${item}</p>`
-// })
+// const diasMapeados = dias.map(mapearCosas)
+
+// function mapearCosas(item) {
+//     return `<p>${item}</p>`;
+// }
 
 // console.log(diasMapeados)
 
+// const saludar = item => console.log(item)
+
+
 // const diasFiltrados = dias.filter( item => item.length > 6 )
+// const diasFiltrados = dias.filter( (item) => {
+//     return item.length > 6
+// }  )
+
+// const diasFiltrados = dias.filter( function (item) {
+//     return item.length > 6
+// }  )
 
 // console.log(diasFiltrados)
 
@@ -48,11 +67,13 @@ const dias = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'do
 // dias.forEach((item, index)=> console.log(item, index))
 
 
-// const diasReducidos = dias.reduce((accu, item) => {
-//     console.log(accu, 'accu')
-//     console.log(item, 'item')
+// const diasReducidos = dias.reduce(reduciendoDias, '')
+
+// function reduciendoDias(accu, item) {
+//     // console.log(accu, 'accu')
+//     // console.log(item, 'item')
 //     return accu + item + ' ';
-// }, '')
+// }
 
 // console.log(diasReducidos)
 
@@ -65,30 +86,30 @@ const dias = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'do
 //     console.log("ejemplo")
 // }
 
-function uno() {
-     console.log("uno")
-}
- function dos() {
-    console.log("dos")
-}
-function tres() {
-    console.log("tres")
-}
+// function uno() {
+//      console.log("uno")
+// }
+//  function dos() {
+//     console.log("dos")
+// }
+// function tres() {
+//     console.log("tres")
+// }
 
-setTimeout(uno, 1000)
-setTimeout(dos, 0)
-tres()
+// setTimeout(uno, 1000)
+// setTimeout(dos, 0)
+// tres()
 
 
-function a() {
-    b()
-    c()
-}
-function b() {
-    c()
-}
-function c() {
-    console.log("holaaaaaa");
-}
+// function a() {
+//     b()
+//     c()
+// }
+// function b() {
+//     c()
+// }
+// function c() {
+//     console.log("holaaaaaa");
+// }
 
-a()
+// a()
---
+++ clase04/index.js
@@ -0,0 +1,98 @@
+let soyUnaVariable = false
+const ajax = new XMLHttpRequest();
+
+ajax.open('GET', 'api/json.json');
+
+ajax.addEventListener('load', ajaxCallback)
+
+ajax.send();
+
+function ajaxCallback() {
+    if (ajax.status === 200) {
+        const respuesta = ajax.response;
+        const respuestaParseada = JSON.parse(respuesta);
+        // console.log(respuestaParseada.data[0].nombre)
+        // soyUnaVariable = true
+        // console.log(soyUnaVariable)
+
+        crearHistorial(respuestaParseada.data)
+    }
+}
+
+
+function crearHistorial(data) {
+    const elementos = data.filter((item)=> !item.deleted );
+    const elementosMapeados = elementos.map(item => crearMensaje(item));
+    mostrar(elementosMapeados);
+}
+
+function mostrar(el) {
+    const mensajes = el.reduce((acu, item)=> {
+        acu.appendChild(item)
+        return acu;
+    }, document.createDocumentFragment())
+    document.body.appendChild(mensajes);
+}
+
+
+function crearMensaje(msj) {
+    const contenedor = document.createElement('div');
+    contenedor.classList.add('contenedor');
+
+    const nombre = document.createElement('p');
+    nombre.classList.add('nombre');
+    nombre.textContent = msj.nombre;
+
+    const contenido = document.createElement('p');
+    contenido.classList.add('contenido');
+    contenido.textContent = msj.mensaje;
+
+    contenedor.appendChild(nombre);
+    contenedor.appendChild(contenido);
+
+    return contenedor;
+}
+
+
+
+
+let obj = {
+    "data": [
+        {
+            "nombre": "Carlitos",
+            "mensaje": "Hola bb",
+            "deleted": true
+        },
+        {
+            "nombre": "Carlitos",
+            "mensaje": "Como estás?",
+            "deleted": false
+        },
+        {
+            "nombre": "Carlitos",
+            "mensaje": "Hola bbbbbbbbbbbbbb",
+            "deleted": true
+        },
+        {
+            "nombre": "Carlitos",
+            "mensaje": "respondemeeee!!!!!!",
+            "deleted": false
+        }
+    ]
+}
+
+// console.log(JSON.stringify(obj))
+
+
+/**
+ * La tareita
+ * 
+ * Consuman una api (igual que hicimos hoy)
+ * 
+ * La api debería preveerles un array con muchos productos
+ * 
+ * Muestren, ni necesidad de mostrarlo muy lindo, los productos. Y... channnn... 
+ * 
+ * Crear un boton de filtro a gusto
+ * 
+ */
